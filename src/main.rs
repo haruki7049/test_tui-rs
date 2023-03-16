c7b4228895496686999b85a5f3ede8e6210f4de4
haruki7049 <tontonkirikiri@gmail.com>
2023-03-16 17:12:22 +0900

    i display the block!!

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,43 @@
-use std::{io, thread, time::Duration};
+use std::io;
 use tui::{
-    backend::CrosstermBackend,
+    style::{Style, Color},
+    backend::{CrosstermBackend, Backend},
     widgets::{Widget, Block, Borders},
     layout::{Layout, Constraint, Direction},
-    Terminal
+    Terminal,
+    Frame,
 };
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    event::{self, Event, KeyCode, KeyEvent, EnableMouseCapture},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
 fn main() -> Result<(), io::Error> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(ui)?;
+
     while let Event::Key(KeyEvent {code, .. }) = event::read()? {
         match code {
             KeyCode::Enter => break,
+            _ => {}
         }
     }
+
+    disable_raw_mode()?;
     Ok(())
+}
+
+fn ui<B: Backend> (f: &mut Frame<B>) {
+    let size = f.size();
+    let block = Block::default()
+        .title("please enter any key")
+        .borders(Borders::ALL);
+    let brdr_style = Style::default().bg(Color::Cyan);
+    f.render_widget(block, size);
 }
