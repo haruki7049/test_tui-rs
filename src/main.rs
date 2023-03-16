625e9843d163a0afd91c08c5bc610913eb326efd
haruki7049 <tontonkirikiri@gmail.com>
2023-03-16 16:32:49 +0900

    hoge

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+fn main() -> Result<(), io::Error> {
+    while let Event::Key(KeyEvent {code, .. }) = event::read()? {
+        match code {
+            KeyCode::Enter => break,
+        }
+    }
+    Ok(())
 }
